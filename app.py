6e2ac1b15758851ff3a3c0ace7f4f23e40577b77
adispen <aedan.dispenza@gmail.com>
2016-04-17 01:34:00 -0400

    food entered into db. style fixes

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = MongoClient()
+db = client.ccDatabase
+foods = db.foods
+people = db.people
+meals = db.meals
 
 name=None
 
@@ -10,6 +18,8 @@ def login_page():
 		if request.form['username']=='kaitcov' and request.form['password']=='p':
 			global name
 			name=request.form['username']
+			post = {"name": name}
+			user_id = people.insert_one(post).inserted_id
 			return redirect(url_for('home', name=name))
 		else:
 			error="Invalid Credentials"
@@ -23,8 +33,19 @@ def home():
 def about():
 	return render_template('about.html', name=name)
 
-@app.route('/createFood')
+@app.route('/createFood', methods=['GET', 'POST'])
 def createFood():
+	if request.method == 'POST':
+		foodName = request.form['foodName']
+		foodSize = request.form['foodSize']
+		foodCarbs = request.form['foodCarbs']
+		foodFiber = request.form['foodFiber']
+		foodCalories = request.form['foodCalories']
+		post = {"user":name, "foodName":foodName, "foodSize":foodSize, "foodCarbs":foodCarbs, "foodFiber":foodFiber, "foodCalories":foodCalories}
+		food_id = foods.insert_one(post).inserted_id
+		for item in foods.find({"name":name}):
+			print item
+
 	return render_template('createFood.html', name=name)
 
 if __name__ == '__main__':
